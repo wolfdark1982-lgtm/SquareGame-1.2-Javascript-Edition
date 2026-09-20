f74f63a6d19f3076849e8f2057b15f1372e54b62
User123573 <wolfdark1982@hotmail.com>
2026-09-20 14:07:09 -0400

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile(path.join(__dirname, '../index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error loading index.html');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } 
+    else if (req.url === '/style.css') {
+        fs.readFile(path.join(__dirname, '../style.css'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error loading style.css');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+    }
+    else if (req.url === '/player.js') {
+        fs.readFile(path.join(__dirname, '../player.js'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error loading player.js');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'application/javascript' });
+            res.end(data);
+        });
+    } 
+    else if (req.url === '/settings.json') {
+        fs.readFile(path.join(__dirname, '../settings.json'), 'utf8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: "Could not read settings.json" }));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(data);
+        });
+    } 
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
